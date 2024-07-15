b16d98e7b5ac448eea4e831fc27e30553660b343
snowykte0426 <snowykte0426@naver.com>
2024-07-15 19:58:25 +0900

    SwaggerConfig 작성

---
+++ src/main/kotlin/Rainbow_Frends/global/SwaggerConfig.kt
@@ -0,0 +1,14 @@
+package Rainbow_Frends.config
+
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun timeApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder().group("times").pathsToMatch("/times/**")
+            .packagesToScan("Rainbow_Frends.domain.times").build()
+    }
+}
