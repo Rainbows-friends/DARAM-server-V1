273d8c387696dc247cc0af018f4050e0f5e70c05
snowykte0426 <snowykte0426@naver.com>
2024-07-19 20:20:22 +0900

    📰feat: Account 테이블을 위한 Entity 생성
    학번과 서비스 내부권한을 설정할 테이블용 Entity 작성

---
+++ src/main/kotlin/Rainbow_Frends/domain/account/Account.kt
@@ -0,0 +1,15 @@
+package Rainbow_Frends.domain.account
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+
+@Entity
+class Account {
+    @Id
+    @Column(name = "student_id", unique = true, nullable = false)
+    private var studen_id: Int? = null
+
+    @Column(name = "role", nullable = false)
+    private var role: Role? = null
+}
---
+++ src/main/kotlin/Rainbow_Frends/domain/account/Role.kt
@@ -0,0 +1,5 @@
+package Rainbow_Frends.domain.account
+
+enum class Role {
+    ROLE_HOUSEFATHER, ROLE_TEACHER, ROLE_DORMITORY_MANAGERx, ROLE_AVERAGE_STUDENT
+}
