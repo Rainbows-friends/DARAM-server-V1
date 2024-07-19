5c6b5a7a5672b6436b29f4c4c9604201a91f545e
snowykte0426 <snowykte0426@naver.com>
2024-07-19 10:30:35 +0900

    🔨refactor: 코드 재정렬
    가독성 향상을 위한 코드 정리

--- src/main/kotlin/Rainbow_Frends/global/GAuth/GAuthService.kt
+++ src/main/kotlin/Rainbow_Frends/global/GAuth/GAuthService.kt
@@ -10,7 +10,7 @@ class GAuthService(private val gAuth: GAuth) {
     val ClientSecret = "39bd0f9270d14cda89a55c54cbffbcb632c4f43b0e184fc1aa7ab5e5c801151d"
     val redirectURI = "https://localhost:5000/page"
     fun fetchAccessToken(code: String): Unit {
-        val accass_token:GAuthToken=gAuth.generateToken(code, ClientID, ClientSecret, redirectURI)
+        val accass_token: GAuthToken = gAuth.generateToken(code, ClientID, ClientSecret, redirectURI)
         println(accass_token)
     }
 }
