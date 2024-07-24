4bb49d71d2f7a94d260c2d0b5a6b168f07830180
snowykte0426 <snowykte0426@naver.com>
2024-07-24 23:31:14 +0900

    🔧fix: SecurityConfig 구성 수정
    대규모 파일 삭제로 인한 오류 방지 겸 수정

--- src/main/kotlin/Rainbow_Frends/global/config/SecurityConfig.kt
+++ src/main/kotlin/Rainbow_Frends/global/config/SecurityConfig.kt
@@ -1,29 +1,20 @@
 package Rainbow_Frends.global.config
 
-import Rainbow_Frends.global.GAuth.JWT.CustomAuthenticationSuccessHandler
-import Rainbow_Frends.global.GAuth.JWT.JwtAuthenticationFilter
-import dev.yangsijun.gauth.configurer.GAuthLoginConfigurer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
-class SecurityConfig(
-    private val gauth: GAuthLoginConfigurer<HttpSecurity>, private val jwtAuthenticationFilter: JwtAuthenticationFilter
-) {
+class SecurityConfig {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        gauth.loginPageUrl("/gauth/authorization").successHandler(CustomAuthenticationSuccessHandler()).configure(http)
-
         http.csrf { it.disable() }.cors { it.disable() }.authorizeHttpRequests { request ->
-            request.requestMatchers(
-                "/gauth/authorization", "/swagger-ui/**", "/v3/api-docs/**", "/times/**"
-            ).permitAll().requestMatchers("/role/student").hasAuthority("ROLE_STUDENT").requestMatchers("/role/teacher")
-                .hasAuthority("ROLE_TEACHER").anyRequest().denyAll()
-        }.addFilterAfter(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
+                request.requestMatchers(
+                    "/swagger-ui/**", "/v3/api-docs/**"
+                ).permitAll().anyRequest().denyAll()
+            }
 
         return http.build()
     }
