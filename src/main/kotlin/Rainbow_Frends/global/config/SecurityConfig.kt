ee883975987c41107d240e42774679629e49ba53
snowykte0426 <snowykte0426@naver.com>
2024-08-21 14:34:13 +0900

    🔧refactor: SecurityConfig 통합 및 apply 메서드 제거에 따른 리팩토링
    SecurityConfig를 통합하고 Spring Security 6.2 이상에서 지원이 중단된 apply 메서드를 제거하여 GAuthLoginConfigurer를 직접 호출하는 방식으로 수정

--- src/main/kotlin/Rainbow_Frends/global/config/SecurityConfig.kt
+++ src/main/kotlin/Rainbow_Frends/global/config/SecurityConfig.kt
@@ -1,21 +1,29 @@
 package Rainbow_Frends.global.config
 
+import dev.yangsijun.gauth.configurer.GAuthLoginConfigurer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
-class SecurityConfig {
+@EnableWebSecurity
+class SecurityConfig(
+    private val gAuthLoginConfigurer: GAuthLoginConfigurer<HttpSecurity>
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http.csrf { it.disable() }.cors { it.disable() }.authorizeHttpRequests { request ->
-                request.requestMatchers(
-                    "/swagger-ui/**", "/v3/api-docs/**"
-                ).permitAll().anyRequest().denyAll()
-            }
+            request.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .requestMatchers("/gauth/authorization", "/login/gauth/code").permitAll().requestMatchers("/auth/me")
+                .authenticated().requestMatchers("/role/student").hasAuthority("GAUTH_ROLE_STUDENT")
+                .requestMatchers("/role/teacher").hasAuthority("GAUTH_ROLE_TEACHER").anyRequest().denyAll()
 
+        }.logout { it.logoutRequestMatcher(AntPathRequestMatcher("/logout")).permitAll() }
+        gAuthLoginConfigurer.configure(http)
         return http.build()
     }
 }
