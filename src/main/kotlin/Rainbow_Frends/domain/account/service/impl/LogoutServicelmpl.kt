1f788afa38fd6db1f9a6f196ee95d5f490ba9325
snowykte0426 <snowykte0426@naver.com>
2024-08-21 20:13:31 +0900

    ⚠️fix: 만료된 리프레시 토큰 예외 클래스 추가
    만료된 리프레시 토큰에 대한 예외를 처리하기 위해 ExpiredRefreshTokenException 클래스를 추가

--- src/main/kotlin/Rainbow_Frends/domain/account/service/impl/LogoutServicelmpl.kt
+++ src/main/kotlin/Rainbow_Frends/domain/account/service/impl/LogoutServicelmpl.kt
@@ -1,7 +1,30 @@
 package Rainbow_Frends.domain.account.service.impl
 
+import Rainbow_Frends.domain.User.entity.User
+import Rainbow_Frends.domain.account.Repository.RefreshRepository
+import Rainbow_Frends.domain.account.Token.RefreshToken
+import Rainbow_Frends.domain.account.exception.ExpiredRefreshTokenException
+import Rainbow_Frends.domain.account.service.LogoutService
+import Rainbow_Frends.domain.account.util.UserUtil
 import Rainbow_Frends.global.annotation.ServiceWithTransaction
+import Rainbow_Frends.global.redis.RedisConfig
+import Rainbow_Frends.global.security.jwt.JwtProvider
 
 @ServiceWithTransaction
-class LogoutServicelmpl {
+class LogoutServiceImpl(
+    private val refreshRepository: RefreshRepository,
+    private val userUtil: UserUtil,
+    private val redisUtil: RedisConfig.RedisUtil,
+    private val jwtProvider: JwtProvider
+) : LogoutService {
+
+    override fun execute(accessToken: String) {
+        val user: User = userUtil.getCurrentUser()
+        val validRefreshToken: RefreshToken = user.id?.let { userId ->
+            refreshRepository.findByMemberId(userId)
+        } ?: throw ExpiredRefreshTokenException()
+
+        refreshRepository.deleteById(validRefreshToken.refreshToken)
+        redisUtil.setBlackList(accessToken, "access_Token", jwtProvider.getExpiration(accessToken))
+    }
 }
