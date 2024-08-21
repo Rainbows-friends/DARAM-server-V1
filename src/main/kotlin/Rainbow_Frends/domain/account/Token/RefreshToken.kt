5816d6d2ff7f29aa8e29540038261db99420db5b
snowykte0426 <snowykte0426@naver.com>
2024-08-21 21:13:35 +0900

    🔨refactor: UserId 타입을 String에서 UUID로 변경
    유저 식별자(UserId)를 더 안전하고 명확하게 사용하기 위해 String에서 UUID로 변경

--- src/main/kotlin/Rainbow_Frends/domain/account/Token/RefreshToken.kt
+++ src/main/kotlin/Rainbow_Frends/domain/account/Token/RefreshToken.kt
@@ -4,8 +4,9 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 import org.springframework.data.redis.core.index.Indexed
 import java.time.LocalDateTime
+import java.util.UUID
 
 @RedisHash(value = "RefreshToken", timeToLive = 60)
 data class RefreshToken(
-    @Id val refreshToken: String, @Indexed val UserId: String, val expiredAt: LocalDateTime
+    @Id val refreshToken: String, @Indexed val UserId: UUID, val expiredAt: LocalDateTime
 )
