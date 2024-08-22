813e91c1a6ff43db67b3570c1aa917116e089d72
snowykte0426 <snowykte0426@naver.com>
2024-08-22 21:16:56 +0900

    🔒chore: Spring Security 설정 업데이트로 API 경로 접근 제어 추가
    새로운 API 경로(`/gauth/authorization`, `/login/gauth/code`, `/login/gauth/logout`, `/login/gauth/reissue`)에 대한 접근 제어를 설정하고, 인증 및 필터링 로직을 개선

--- src/main/kotlin/Rainbow_Frends/global/config/SecurityConfig.kt
+++ src/main/kotlin/Rainbow_Frends/global/config/SecurityConfig.kt
@@ -1,28 +1,37 @@
 package Rainbow_Frends.global.config
 
+import Rainbow_Frends.global.security.filter.JwtFilter
+import Rainbow_Frends.global.security.jwt.JwtProvider
 import dev.yangsijun.gauth.configurer.GAuthLoginConfigurer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig(
-    private val gAuthLoginConfigurer: GAuthLoginConfigurer<HttpSecurity>
+    private val gAuthLoginConfigurer: GAuthLoginConfigurer<HttpSecurity>, private val jwtProvider: JwtProvider
 ) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.csrf { it.disable() }.cors { it.disable() }.authorizeHttpRequests { request ->
-            request.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                .requestMatchers("/gauth/authorization", "/login/gauth/code","/role/student").permitAll().requestMatchers("/auth/me")
-                //.authenticated().requestMatchers("/role/student").hasAuthority("GAUTH_ROLE_STUDENT")
-                //.requestMatchers("/role/teacher").hasAuthority("GAUTH_ROLE_TEACHER").anyRequest().denyAll()
+        http.csrf { it.disable() }.cors { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { request ->
+                request.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll().requestMatchers(
+                        "/gauth/authorization",
+                        "/login/gauth/code",
+                        "login/gauth/logout",
+                        "login/gauth/reissue"
+                    ).permitAll().requestMatchers("/auth/me", "times/remaintime").authenticated()
+                    .requestMatchers("/role/student").hasAuthority("GAUTH_ROLE_STUDENT")
+                    .requestMatchers("/role/teacher").hasAuthority("GAUTH_ROLE_TEACHER").anyRequest().denyAll()
+            }.addFilterBefore(JwtFilter(jwtProvider), UsernamePasswordAuthenticationFilter::class.java)
 
-        }.logout { it.logoutRequestMatcher(AntPathRequestMatcher("/logout")).permitAll() }
         gAuthLoginConfigurer.configure(http)
         return http.build()
     }
