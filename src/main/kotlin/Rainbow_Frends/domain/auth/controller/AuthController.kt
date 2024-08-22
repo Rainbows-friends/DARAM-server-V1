cc1576501c2e42ed3debeb45b70b57230e0fa598
snowykte0426 <snowykte0426@naver.com>
2024-08-22 13:07:06 +0900

    ✨feat: 토큰 재발급 및 로그아웃 API 엔드포인트 추가
    ReissueTokenService와 LogoutService를 활용하여 토큰 재발급(`/reissue`) 및 로그아웃(`/logout`) 기능을 제공하는 API 엔드포인트를 추가

--- src/main/kotlin/Rainbow_Frends/domain/auth/controller/AuthController.kt
+++ src/main/kotlin/Rainbow_Frends/domain/auth/controller/AuthController.kt
@@ -2,29 +2,47 @@ package Rainbow_Frends.domain.auth.controller
 
 import Rainbow_Frends.domain.account.presentation.dto.request.SignInRequest
 import Rainbow_Frends.domain.account.presentation.dto.response.TokenResponse
+import Rainbow_Frends.domain.account.service.LogoutService
+import Rainbow_Frends.domain.account.service.ReissueTokenService
 import Rainbow_Frends.domain.account.service.SignInService
-import Rainbow_Frends.domain.account.service.impl.SignInServiceImpl
-import gauth.*
+import jakarta.servlet.http.HttpServletRequest
 import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
-@RequestMapping("/login/gauth/code")
+@RequestMapping("/login/gauth")
 @RestController
-class AuthController(private val signInService: SignInService) {
+class AuthController(
+    private val signInService: SignInService,
+    private val logoutService: LogoutService,
+    private val reissueTokenService: ReissueTokenService
+) {
+
     @Value("\${GAuth-CLIENT-ID}")
-    private val clientId: String? = null
+    private lateinit var clientId: String
 
     @Value("\${GAuth-CLIENT-SECRET}")
-    private val clientSecret: String? = null
+    private lateinit var clientSecret: String
 
     @Value("\${GAuth-REDIRECT-URI}")
-    private val redirectUri: String? = null
-    @PostMapping
+    private lateinit var redirectUri: String
+
+    @PostMapping("/code")
     fun signIn(@RequestBody @Valid signInRequest: SignInRequest): ResponseEntity<TokenResponse> {
         val response = signInService.execute(signInRequest)
         return ResponseEntity.ok(response)
     }
 
+    @PatchMapping("/reissue")
+    fun reissueToken(@RequestHeader("Authorization") refreshToken: String): ResponseEntity<TokenResponse> {
+        val tokenResponse: TokenResponse = reissueTokenService.execute(refreshToken)
+        return ResponseEntity.ok(tokenResponse)
+    }
+
+    @DeleteMapping("/logout")
+    fun logout(request: HttpServletRequest): ResponseEntity<Void> {
+        logoutService.execute(request.getHeader("Authorization").substring(7))
+        return ResponseEntity.noContent().build()
+    }
 }
