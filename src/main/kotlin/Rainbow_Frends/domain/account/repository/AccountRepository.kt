7969220ddabfe9388c1775ffb3efdb73b32de289
snowykte0426 <snowykte0426@naver.com>
2024-08-29 21:13:17 +0900

    🔥chore: AccountRepository 삭제 처리
    `AccountRepository` 파일이 위치 변경으로 인해 삭제 처리되었습니다. 이후 재구성된 경로에 맞춰 커밋을 진행했습니다.

--- src/main/kotlin/Rainbow_Frends/domain/account/repository/AccountRepository.kt
+++
@@ -1,7 +0,0 @@
-package Rainbow_Frends.domain.account.repository
-
-import Rainbow_Frends.domain.account.entity.Account
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AccountRepository: JpaRepository<Account, Long> {
-}
