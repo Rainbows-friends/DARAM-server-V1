c767dc1a7423625df551d9bf25754fc0b1c64ee3
snowykte0426 <snowykte0426@naver.com>
2024-08-29 21:16:46 +0900

    🔥chore: RefreshRepository 삭제 처리
    `RefreshRepository` 파일이 위치 변경으로 인해 삭제 처리되었습니다. 새로운 경로에 맞춰 리포지토리를 재구성했습니다.

--- src/main/kotlin/Rainbow_Frends/domain/account/repository/RefreshRepository.kt
+++
@@ -1,9 +0,0 @@
-package Rainbow_Frends.domain.account.repository
-
-import Rainbow_Frends.domain.account.Token.RefreshToken
-import org.springframework.data.repository.CrudRepository
-import java.util.*
-
-interface RefreshRepository : CrudRepository<RefreshToken, String> {
-    fun findByUserId(memberId: UUID): RefreshToken?
-}
