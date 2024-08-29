993e61d12625e9c5259771809c2efa25d418d5ef
snowykte0426 <snowykte0426@naver.com>
2024-08-29 21:14:19 +0900

    ✨feat: JPA 및 Redis 리포지토리 활성화 설정 추가
    `SecurityApplication` 클래스에 `@EnableJpaRepositories`와 `@EnableRedisRepositories` 어노테이션을 추가하여 JPA 및 Redis 리포지토리를 활성화했습니다. 이를 통해 JPA 리포지토리와 Redis 리포지토리를 명시적으로 관리할 수 있도록 설정했습니다.

--- src/main/kotlin/Rainbow_Frends/Application.kt
+++ src/main/kotlin/Rainbow_Frends/Application.kt
@@ -2,8 +2,12 @@ package Rainbow_Frends
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = ["Rainbow_Frends.domain.account.repository.jpa", "Rainbow_Frends.domain.User.repository"])
+@EnableRedisRepositories(basePackages = ["Rainbow_Frends.domain.account.repository.redis"])
 class SecurityApplication
 
 fun main(args: Array<String>) {
