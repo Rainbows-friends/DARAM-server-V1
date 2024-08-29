77559789f800a5d2d104cc58619ef3ceb226324e
snowykte0426 <snowykte0426@naver.com>
2024-08-29 21:13:52 +0900

    ♻️refactor: Account 엔티티 수정 및 필드 접근 제한 해제
    `Account` 엔티티의 필드 `student_id`와 `role`의 접근 제한을 해제하여 `private`에서 `var`로 변경했습니다. 또한, `role` 필드에 `@Enumerated(EnumType.STRING)` 어노테이션을 추가하여 열거형 값을 문자열로 저장하도록 설정했습니다.

--- src/main/kotlin/Rainbow_Frends/domain/account/entity/Account.kt
+++ src/main/kotlin/Rainbow_Frends/domain/account/entity/Account.kt
@@ -1,15 +1,14 @@
 package Rainbow_Frends.domain.account.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
 class Account {
     @Id
     @Column(name = "student_id", unique = true, nullable = false)
-    private var studen_id: Int? = null
+    var student_id: Int? = null
 
     @Column(name = "role", nullable = false)
-    private var role: Role? = null
+    @Enumerated(EnumType.STRING)
+    var role: Role? = null
 }
