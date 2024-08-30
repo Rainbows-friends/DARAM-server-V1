5951fe150e4b2c2cae6cb2640ae340bd44d10334
snowykte0426 <snowykte0426@naver.com>
2024-08-30 15:41:27 +0900

    ✨feat: Swagger 설정에 Notice API 그룹 추가
    Swagger 설정에 Notice API를 위한 그룹을 추가하여, /api/notice/** 경로에 대한 API 문서를 별도로 관리할 수 있도록 했습니다.

--- src/main/kotlin/Rainbow_Frends/global/config/SwaggerConfig.kt
+++ src/main/kotlin/Rainbow_Frends/global/config/SwaggerConfig.kt
@@ -11,9 +11,16 @@ class SwaggerConfig {
         return GroupedOpenApi.builder().group("times").pathsToMatch("/api/times/**")
             .packagesToScan("Rainbow_Frends.domain.times.controller").build()
     }
+
     @Bean
     fun gauthApi(): GroupedOpenApi {
         return GroupedOpenApi.builder().group("GAuth").pathsToMatch("/api/login/gauth/**")
             .packagesToScan("Rainbow_Frends.domain.auth.controller").build()
+    }
+
+    @Bean
+    fun noticeApi(): GroupedOpenApi {
+        return GroupedOpenApi.builder().group("Notice").pathsToMatch("/api/notice/**")
+            .packagesToScan("Rainbow_Frends.domain.notice.controller").build()
     }
 }
