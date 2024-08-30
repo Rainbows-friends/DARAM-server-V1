ac3123d3f6cab2b92416f3fb4e22a1d2fc95ea6b
snowykte0426 <snowykte0426@naver.com>
2024-08-31 02:41:08 +0900

    ✨feat: 프로필 사진 업데이트를 위한 ProfilePictureService 인터페이스 추가
    프로필 사진을 업데이트하기 위한 ProfilePictureService 인터페이스를 추가했습니다. 이 인터페이스는 Account 엔티티와 파일을 받아 프로필 사진을 업데이트하는 기능을 정의합니다.

---
+++ src/main/kotlin/Rainbow_Frends/domain/account/service/ProfilePictureService.kt
@@ -0,0 +1,9 @@
+package Rainbow_Frends.domain.account.service
+
+import Rainbow_Frends.domain.User.entity.User
+import Rainbow_Frends.domain.account.entity.Account
+import org.springframework.web.multipart.MultipartFile
+
+interface ProfilePictureService {
+    fun updateProfilePicture(account: Account, file: MultipartFile)
+}
