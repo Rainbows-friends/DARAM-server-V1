27f5d51f186ae05749afd0db9d3a67c0724b55cd
snowykte0426 <snowykte0426@naver.com>
2024-08-30 15:36:15 +0900

    ✨ feat: AccountRepository에 findByStudentId 메서드 추가
    AccountRepository에 studentId로 Account를 조회할 수 있는 findByStudentId 메서드를 추가했습니다.

--- src/main/kotlin/Rainbow_Frends/domain/account/repository/jpa/AccountRepository.kt
+++ src/main/kotlin/Rainbow_Frends/domain/account/repository/jpa/AccountRepository.kt
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface AccountRepository: JpaRepository<Account, Int>
+interface AccountRepository : JpaRepository<Account, Int> {
+    fun findByStudentId(studentId: Int): Account?
+}
