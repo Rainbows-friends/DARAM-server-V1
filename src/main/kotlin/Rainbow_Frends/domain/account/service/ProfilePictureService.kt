f1fe5866fb34a9e2409f929ac2e8f33417c82b8a
snowykte0426 <snowykte0426@naver.com>
2024-08-31 19:33:18 +0900

    ♻️refactor: ProfilePictureService 인터페이스에서 불필요한 import 제거
    ProfilePictureService 인터페이스에서 사용되지 않는 User 엔티티의 import 문을 제거하여 코드의 간결성과 유지보수성을 개선했습니다.

--- src/main/kotlin/Rainbow_Frends/domain/account/service/ProfilePictureService.kt
+++ src/main/kotlin/Rainbow_Frends/domain/account/service/ProfilePictureService.kt
@@ -1,6 +1,5 @@
 package Rainbow_Frends.domain.account.service
 
-import Rainbow_Frends.domain.User.entity.User
 import Rainbow_Frends.domain.account.entity.Account
 import org.springframework.web.multipart.MultipartFile
 
