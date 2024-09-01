b02a211c4bdda68c9ddd5b68d1b67d3daf2d71e5
snowykte0426 <snowykte0426@naver.com>
2024-09-01 13:21:46 +0900

    ✨feat: 개발 환경을 위한 DevFilter 추가
    DEV_KEY를 이용하여 개발 환경에서 특정 유저의 인증을 모의하는 DevFilter를 추가했습니다. 이 필터는 "Authorization" 헤더에 DEV_KEY가 포함된 요청에 대해 "email" 헤더를 확인하고, 해당 유저의 인증 토큰을 생성하여 응답에 포함시킵니다. "email" 헤더가 없을 경우 400 Bad Request를 반환합니다.

---
+++ src/main/kotlin/Rainbow_Frends/global/security/filter/DevFilter.kt
@@ -0,0 +1,46 @@
+package Rainbow_Frends.global.security.filter
+
+import Rainbow_Frends.domain.User.repository.UserRepository
+import Rainbow_Frends.global.security.jwt.JwtProvider
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+
+@Component
+class DevFilter(
+    @Value("\${DEV_KEY}") private val devKey: String,
+    private val jwtProvider: JwtProvider,
+    private val userRepository: UserRepository
+) : OncePerRequestFilter() {
+    @Throws(ServletException::class, IOException::class)
+    override fun doFilterInternal(
+        request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain
+    ) {
+        val authorizationHeader = request.getHeader("Authorization")
+        val emailHeader = request.getHeader("email")
+        if (authorizationHeader == devKey) {
+            if (emailHeader != null) {
+                val user = userRepository.findByEmail(emailHeader)
+                val authentication =
+                    user?.id?.let { jwtProvider.generateAccessToken(it) }?.let { jwtProvider.getAuthentication(it) }
+                SecurityContextHolder.getContext().authentication = authentication
+                if (user != null) {
+                    response.setHeader(
+                        "Authorization",
+                        "Bearer " + user.id?.let { jwtProvider.generateAccessToken(it) })
+                }
+            } else {
+                response.status = HttpServletResponse.SC_BAD_REQUEST
+                response.writer.write("Missing email header")
+                return
+            }
+        }
+        filterChain.doFilter(request, response)
+    }
+}
