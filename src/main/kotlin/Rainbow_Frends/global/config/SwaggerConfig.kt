8c71b69e33a35c2d7fcfaeae73038f6b37702e62
snowykte0426 <snowykte0426@naver.com>
2024-09-04 08:44:58 +0900

    ✨ feat: SwaggerConfig에 customOpenAPI 메서드 추가 및 GAuth 경로 정의
    Swagger 설정 파일에 customOpenAPI 설정을 추가하여 API 문서화 기능을 개선하였습니다. 기존의 GroupedOpenApi 설정은 유지하면서, GAuth 로그인 페이지에 대한 API 경로와 설명을 OpenAPI 설정에 포함시켰습니다. 이를 통해 API 문서의 명확성과 가독성을 높였습니다.

--- src/main/kotlin/Rainbow_Frends/global/config/SwaggerConfig.kt
+++ src/main/kotlin/Rainbow_Frends/global/config/SwaggerConfig.kt
@@ -1,11 +1,20 @@
 package Rainbow_Frends.global.config
 
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.PathItem
+import io.swagger.v3.oas.models.Paths
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.parameters.Parameter
+import io.swagger.v3.oas.models.responses.ApiResponse
+import io.swagger.v3.oas.models.responses.ApiResponses
+import io.swagger.v3.oas.models.tags.Tag
 import org.springdoc.core.models.GroupedOpenApi
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfig {
+
     @Bean
     fun timeApi(): GroupedOpenApi {
         return GroupedOpenApi.builder().group("Time").pathsToMatch("/api/times/**")
@@ -34,5 +43,26 @@ class SwaggerConfig {
     fun checkinApi(): GroupedOpenApi {
         return GroupedOpenApi.builder().group("Checkin").pathsToMatch("/api/checkin/**")
             .packagesToScan("Rainbow_Frends.domain.checkin.controller").build()
+    }
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        val gauthPathItem = PathItem().get(
+            io.swagger.v3.oas.models.Operation().addTagsItem("인증").summary("로그인 페이지").addParametersItem(
+                    Parameter().`in`("query").name("param").description("GAuth 로그인 페이지를 반환합니다")
+                ).responses(
+                    ApiResponses().addApiResponse(
+                        "200", ApiResponse().description("Successful GAuth authorization")
+                    )
+                )
+        )
+
+        return OpenAPI().info(
+            Info().title("Rainbow Frends API").version("v1.0")
+        ).paths(
+            Paths().addPathItem("/gauth/authorization", gauthPathItem)
+        ).addTagsItem(
+            Tag().name("인증").description("인증 관련 API")
+        )
     }
 }
