954d83a1e4f1af96821b1e1973ff50690d09b2dd
snowykte0426 <snowykte0426@naver.com>
2024-09-04 20:36:35 +0900

    🔨 refactor: NoticeService 인터페이스 수정
    NoticeService 인터페이스에 공지 수정, 삭제, 단일 공지 조회 기능을 추가했습니다. 이제 공지를 조회하는 기능 외에도 공지의 수정 및 삭제, 특정 공지 조회가 가능하도록 메서드를 확장했습니다.

--- src/main/kotlin/Rainbow_Frends/domain/notice/service/NoticeService.kt
+++ src/main/kotlin/Rainbow_Frends/domain/notice/service/NoticeService.kt
@@ -1,7 +1,12 @@
 package Rainbow_Frends.domain.notice.service
 
+import Rainbow_Frends.domain.account.entity.Account
+import Rainbow_Frends.domain.notice.dto.request.NoticeRequest
 import Rainbow_Frends.domain.notice.entity.Notice
 
 interface NoticeService {
     fun readAllNotice(): List<Notice>
+    fun findNoticeById(id: Long): Notice?
+    fun deleteNotice(id: Long): Boolean
+    fun updateNotice(id: Long, noticeRequest: NoticeRequest, account: Account): Notice?
 }
