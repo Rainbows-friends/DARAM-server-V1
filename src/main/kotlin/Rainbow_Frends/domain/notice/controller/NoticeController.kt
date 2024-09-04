0ba603ffb14986803555d279c110ea080e3d4e81
snowykte0426 <snowykte0426@naver.com>
2024-09-04 20:35:41 +0900

    ✨ feat: NoticeController에 공지 수정 및 삭제 기능 추가
    NoticeController에 공지 수정 및 삭제 API를 추가했습니다. 공지사항을 조회하는 기능 외에도 특정 공지에 대한 수정 및 삭제를 가능하게 하여 관리자 권한을 가진 사용자가 공지를 관리할 수 있도록 했습니다.

--- src/main/kotlin/Rainbow_Frends/domain/notice/controller/NoticeController.kt
+++ src/main/kotlin/Rainbow_Frends/domain/notice/controller/NoticeController.kt
@@ -1,5 +1,6 @@
 package Rainbow_Frends.domain.notice.controller
 
+import Rainbow_Frends.domain.account.entity.Account
 import Rainbow_Frends.domain.account.repository.jpa.AccountRepository
 import Rainbow_Frends.domain.notice.dto.request.NoticeRequest
 import Rainbow_Frends.domain.notice.entity.Notice
@@ -38,5 +39,49 @@ class NoticeController(
         val userDetails = authentication.principal as UserDetails
         val user = noticeService.getUserByUsername(userDetails.username)
         return noticeService.createNotice(noticeRequest, user)
+    }
+
+    @Operation(summary = "특정 공지 조회 API", description = "특정 공지의 제목, 글쓴이, 본문, 작성시각 조회 API")
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/{id}")
+    fun getNotice(@PathVariable id: Long): ResponseEntity<Notice> {
+        val notice = noticeService.findNoticeById(id)
+        return if (notice != null) {
+            ResponseEntity.ok(notice)
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @Operation(summary = "공지 수정 API", description = "공지의 제목, 본문 등을 수정하는 API")
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping("/{id}")
+    fun updateNotice(
+        @PathVariable id: Long, @RequestBody noticeRequest: NoticeRequest, request: HttpServletRequest
+    ): ResponseEntity<Notice> {
+        val accessToken = jwtProvider.resolveToken(request)
+        val authentication = jwtProvider.getAuthentication(accessToken!!)
+        val userDetails = authentication.principal as UserDetails
+        val user = noticeService.getUserByUsername(userDetails.username)
+        val studentId =
+            ((user.studentNum!!.grade) * 1000) + ((user.studentNum.classNum) * 100) + ((user.studentNum.number))
+        val account: Account? = accountRepository.findByStudentId(studentId)
+        val updatedNotice = account?.let { noticeService.updateNotice(id, noticeRequest, it) }
+        return if (updatedNotice != null) {
+            ResponseEntity.ok(updatedNotice)
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @Operation(summary = "공지 삭제 API", description = "특정 공지를 삭제하는 API")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    fun deleteNotice(@PathVariable id: Long): ResponseEntity<Void> {
+        return if (noticeService.deleteNotice(id)) {
+            ResponseEntity.noContent().build()
+        } else {
+            ResponseEntity.notFound().build()
+        }
     }
 }
