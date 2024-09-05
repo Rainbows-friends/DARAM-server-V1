b4ea9c08c83cf7bc8ce7ce3fcafa2860c5084899
snowykte0426 <snowykte0426@naver.com>
2024-09-05 20:49:23 +0900

    ♻️refactor: 비즈니스 로직 개선
    비즈니스 로직 개선에 따른 파라미터 변화를 수용하기 위해 ProfilePictureService 인터페이스를 변경했습니다

--- src/main/kotlin/Rainbow_Frends/domain/account/service/ProfilePictureService.kt
+++ src/main/kotlin/Rainbow_Frends/domain/account/service/ProfilePictureService.kt
@@ -1,8 +1,9 @@
 package Rainbow_Frends.domain.account.service
 
 import Rainbow_Frends.domain.account.entity.Account
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.web.multipart.MultipartFile
 
 interface ProfilePictureService {
-    fun updateProfilePicture(account: Account, file: MultipartFile)
+    fun updateProfilePicture(request: HttpServletRequest, file: MultipartFile)
 }
