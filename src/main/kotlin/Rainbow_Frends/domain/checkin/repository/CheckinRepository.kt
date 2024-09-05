09ac7c1755cd7f1464d2b87b7afadc5de4a4bb6f
snowykte0426 <snowykte0426@naver.com>
2024-09-05 20:36:26 +0900

    ✨feat: checkin_info 테이블 findByUser 추가
    CheckinRepository에 findByUser를 추가해 User정보를 기반으로 테이블을 탐색할 수 있도록 하였습니다

--- src/main/kotlin/Rainbow_Frends/domain/checkin/repository/CheckinRepository.kt
+++ src/main/kotlin/Rainbow_Frends/domain/checkin/repository/CheckinRepository.kt
@@ -1,6 +1,6 @@
 package Rainbow_Frends.domain.checkin.repository
 
-import Rainbow_Frends.domain.User.entity.User
+import Rainbow_Frends.domain.user.entity.User
 import Rainbow_Frends.domain.checkin.entity.Checkin
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
@@ -9,6 +9,7 @@ import java.time.LocalDate
 @Repository
 interface CheckinRepository : JpaRepository<Checkin, Long> {
     fun findByUserAndCheckinDate(user: User, checkinDate: LocalDate): List<Checkin>
-    fun findByCheckintruefalseAndCheckinDate(checkintruefalse: Boolean, checkinDate: LocalDate): List<Checkin>
+    fun findByCheckinStatusAndCheckinDate(checkintruefalse: Boolean, checkinDate: LocalDate): List<Checkin>
     fun findByCheckinDateBefore(checkinDate: LocalDate): List<Checkin>
+    fun findByUser(user: User): List<Checkin>
 }
