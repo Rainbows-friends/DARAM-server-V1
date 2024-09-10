0bc64400a7ec8a57c42b43702e696627f4ba6e08
snowykte0426 <snowykte0426@naver.com>
2024-09-10 19:43:23 +0900

    ♻️refactor: 불필요 호출조건 삭제
    불필요한 안전한 호출을 마침표 호출로 변경했습니다

--- src/main/kotlin/Rainbow_Frends/global/auth/GetStudentId.kt
+++ src/main/kotlin/Rainbow_Frends/global/auth/GetStudentId.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component
 @Component
 class GetStudentId(private val userRepository: UserRepository) {
     fun getStudentId(email:String): Int {
-        val user = email?.let { userRepository.findByEmail(it) } ?: throw RuntimeException("이메일로 사용자를 찾을 수 없습니다: $email")
+        val user = email.let { userRepository.findByEmail(it) } ?: throw RuntimeException("이메일로 사용자를 찾을 수 없습니다: $email")
         val studentNum = user.studentNum?.let {
             (it.grade * 1000) + (it.classNum * 100) + it.number
         } ?: throw RuntimeException("유효하지 않은 학번 정보입니다.")
